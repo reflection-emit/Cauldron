12f055fc12218195c491f685ff27c6b1f3d2d987
Alexei Stryker <evron69@hotmail.com>
2016-04-09 22:11:18 +0200

    ..

--- Themes/Couldron.Themes.VisualStudioLight/Behaviours/ComboBoxBehaviour.cs
+++ Themes/Couldron.Themes.VisualStudioLight/Behaviours/ComboBoxBehaviour.cs
@@ -23,25 +23,4 @@ namespace Couldron.Themes.VisualStudio.Behaviours
                 this.AssociatedObject.IsDropDownOpen = true;
         }
     }
-
-    internal class TreeViewComboBoxBehaviour : Behaviour<ComboBox>
-    {
-        protected override void OnAttach()
-        {
-            this.AssociatedObject.KeyUp += AssociatedObject_KeyUp;
-        }
-
-        protected override void OnDetach()
-        {
-            this.AssociatedObject.KeyUp -= AssociatedObject_KeyUp;
-        }
-
-        private void AssociatedObject_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == System.Windows.Input.Key.Delete)
-                this.AssociatedObject.SelectedItem = null;
-            else if (e.Key == System.Windows.Input.Key.Space)
-                this.AssociatedObject.IsDropDownOpen = true;
-        }
-    }
 }
