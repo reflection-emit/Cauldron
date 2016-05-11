ca011aa870431f605b7e847ed93716d5707e3326
Alexei Stryker <evron69@hotmail.com>
2016-05-11 15:47:47 +0200

    ...

---
+++ Cauldron.UWP/Behaviours/InvocationTrigger.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Markup;
+
+namespace Cauldron.Behaviours
+{
+    /// <summary>
+    /// Provides a Behaviour that can invoke <see cref="ActionBase"/> behaviours using invoke awareness event
+    /// </summary>
+    [ContentProperty(Name = nameof(Actions))]
+    public partial class InvocationTrigger
+    {
+    }
+}
