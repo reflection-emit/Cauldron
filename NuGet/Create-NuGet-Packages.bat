972aba7a0b99e799c66bd4eab8f22f441fe2ce1e
reflection-emit <alexander.schunk@capgemini.com>
2017-06-23 21:21:58 +0200

    NETCore added to Nuget package again Version changed

--- NuGet/Create-NuGet-Packages.bat
+++ NuGet/Create-NuGet-Packages.bat
@@ -31,7 +31,7 @@ goto:eof
 write-colored Green --------------------------
 set nuspec=%~1
 write-colored Cyan Creating NuGet Packages %nuspec%
-nuget.exe pack %nuspec% -OutputDir %~dp0Packages -version 1.2.7
+nuget.exe pack %nuspec% -OutputDir %~dp0Packages -version 1.2.8
 goto:eof
 
 echo on
