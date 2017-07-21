c7d3edf71fcc36f357b607bf469273d0b208f569
reflection-emit <alexander.schunk@capgemini.com>
2017-07-21 09:52:56 +0200

    ...

--- NuGet/Create-NuGet-Packages.bat
+++ NuGet/Create-NuGet-Packages.bat
@@ -31,7 +31,7 @@ goto:eof
 write-colored Green --------------------------
 set nuspec=%~1
 write-colored Cyan Creating NuGet Packages %nuspec%
-nuget.exe pack %nuspec% -OutputDir %~dp0Packages -version 1.2.9-beta
+nuget.exe pack %nuspec% -OutputDir %~dp0Packages -version 1.2.10-beta
 goto:eof
 
 echo on
