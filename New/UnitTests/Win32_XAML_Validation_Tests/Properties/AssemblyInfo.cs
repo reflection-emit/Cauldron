b263242d749e9096f611d25406c1c190d0b3680c
Alexei Stryker <alexander.schunk@capgemini.com>
2017-11-24 00:43:50 +0100

    ...

---
+++ New/UnitTests/Win32_XAML_Validation_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Win32_XAML_Validation_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Win32_XAML_Validation_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf0e64fd-70f3-4bb6-a131-b11d667ed0b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ New/UnitTests/Win32_XAML_Validation_Tests/ValidationTest.cs
@@ -0,0 +1,119 @@
+﻿using Cauldron.Activator;
+using Cauldron.XAML.Validation;
+using Cauldron.XAML.Validation.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cauldron.Test
+{
+    public class SparrowViewModel : ValidatableViewModelBase
+    {
+        [StringLength(5, 10, "key")]
+        public string Caption { get; set; }
+
+        [IsMandatory("mandatory")]
+        public string Name { get; set; }
+
+        [Equality(nameof(Password2), "password does not match")]
+        public string Password { get; set; }
+
+        [Equality(nameof(Password), "password does not match 2")]
+        public string Password2 { get; set; }
+
+        [StringLength(10, "key")]
+        public string Text { get; set; }
+    }
+
+    [TestClass]
+    public class ValidationTest
+    {
+        [TestMethod]
+        public void Equality_Validator_Error_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Password = "Password123";
+            vm.Password2 = "ko";
+
+            Assert.AreEqual(true, vm.HasErrors);
+        }
+
+        [TestMethod]
+        public void Equality_Validator_NoError_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Password = "Password123";
+            vm.Password2 = "Password123";
+
+            Assert.AreEqual(false, vm.HasErrors);
+        }
+
+        [TestMethod]
+        public void Simple_Validation_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Name = "";
+
+            vm.ValidateAsync().RunSync();
+
+            Assert.AreEqual(true, vm.HasErrors);
+        }
+
+        [TestMethod]
+        public void StringLength_Length_Validator_Error_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Text = "Blabla";
+
+            Assert.AreEqual(true, vm.HasErrors);
+        }
+
+        [TestMethod]
+        public void StringLength_Length_Validator_NoErrors_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Text = "0123456789";
+
+            Assert.AreEqual(false, vm.HasErrors);
+        }
+
+        [TestMethod]
+        public void StringLength_Max_Validator_Error_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Caption = "01234567890";
+
+            Task.Delay(2000);
+            Assert.AreEqual(true, vm.HasErrors);
+        }
+
+        [TestMethod]
+        public void StringLength_Min_Validator_Error_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Caption = "0123";
+
+            Assert.AreEqual(true, vm.HasErrors);
+        }
+
+        [TestMethod]
+        public void StringLength_MinMax_Validator_NoErrors_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Caption = "012345678";
+
+            Assert.AreEqual(false, vm.HasErrors);
+        }
+
+        [TestMethod]
+        public void Validation_Message_Test()
+        {
+            var vm = Factory.Create<SparrowViewModel>();
+            vm.Name = "";
+            vm.ValidateAsync().RunSync();
+            var errorMessage = vm.GetErrors(nameof(SparrowViewModel.Name)).Cast<string>().ToArray().Join("\n");
+
+            Assert.IsFalse(string.IsNullOrEmpty(errorMessage));
+        }
+    }
+}
