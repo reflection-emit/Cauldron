fbe2cd5f6add09f5ab9e699acc93b5c0f4a3e7c0
Alexei Stryker <alexander.schunk@capgemini.com>
2017-12-03 08:44:31 +0100

    ...

--- New/Fody/Cauldron.Interception.Cecilator/Properties/AssemblyInfo.cs
+++ New/Fody/Cauldron.Interception.Cecilator/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //
 // Major Version Minor Version Build Number Revision
 //
-[assembly: AssemblyVersion("2.0.0.10")]
-[assembly: AssemblyFileVersion("2.0.0.10")]
+[assembly: AssemblyVersion("2.0.0.11")]
+[assembly: AssemblyFileVersion("2.0.0.11")]
