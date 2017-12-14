73c9f3eff1f943315438752417998b7cc4cb1496
Alexei Stryker <alexander.schunk@capgemini.com>
2017-12-14 02:45:00 +0100

    Interface for Constructor interceptor defined #45

---
+++ NetStandard/Cauldron.Interception/IConstructorInterceptor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cauldron.Interception
+{
+    /// <summary>
+    /// Represents a contructor interceptor.
+    /// <para/>
+    ///
+    /// </summary>
+    public interface IConstructorInterceptor : IInterceptor
+    {
+        /// <summary>
+        /// Invoked before the initialization of the class.
+        /// <para/>
+        /// This happens before the base call which means that the instance has not been created yet. Use this with care.
+        /// </summary>
+        /// <param name="declaringType">The type declaring the intercepted contructor</param>
+        /// <param name="methodbase">Contains information about the constructor.</param>
+        /// <param name="values">The passed arguments of the constructor.</param>
+        void OnBeforeInitialization(Type declaringType, MethodBase methodbase, object[] values);
+
+        /// <summary>
+        /// Invoked if an intercepted contructor has been called.
+        /// </summary>
+        /// <param name="declaringType">The type declaring the intercepted contructor</param>
+        /// <param name="instance">
+        /// The instance of the class where the method is residing. will be null if the constructor is static.
+        /// </param>
+        /// <param name="methodbase">Contains information about the constructor.</param>
+        /// <param name="values">The passed arguments of the constructor.</param>
+        void OnEnter(Type declaringType, object instance, MethodBase methodbase, object[] values);
+
+        /// <summary>
+        /// Invoked if an intercepted construtor has raised an exception. The constructor will always rethrow
+        /// the exception.
+        /// </summary>
+        /// <param name="e">The exception information.</param>
+        void OnException(Exception e);
+
+        /// <summary>
+        /// Invoked if the intercepted constructor has finished executing.
+        /// </summary>
+        void OnExit();
+    }
+}
