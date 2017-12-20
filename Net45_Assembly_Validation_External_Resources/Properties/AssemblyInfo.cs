d059c532f1346459dfb24a6e59379a47425e5849
Alexei Stryker <alexander.schunk@capgemini.com>
2017-12-20 23:35:00 +0100

    Net45 for interception Assembly verification unit tests

--- Net45_Assembly_Validation_External_Resources/Properties/AssemblyInfo.cs
+++ Net45_Assembly_Validation_External_Resources/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cauldron.Win32.Net461.Interception")]
+[assembly: AssemblyTitle("Net45_Assembly_Validation_External_Resources")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alexander Schunk, Capgemini Deutschland GmbH")]
-[assembly: AssemblyProduct("Cauldron.Win32.Net461.Interception")]
-[assembly: AssemblyCopyright("Copyright (c) 2016 Capgemini Deutschland GmbH")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Net45_Assembly_Validation_External_Resources")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f7082ec9-ecae-4e25-b6b5-decd9b61a707")]
+[assembly: Guid("38faa55a-97f0-403a-8431-7810a61a59b4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.0.22")]
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.22")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/Win32_Net45_Assembly_Validation_Tests/Method_Interceptor_Tests.cs
@@ -0,0 +1,89 @@
+﻿using Cauldron.Interception;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+
+namespace Win32_Net45_Assembly_Validation_Tests
+{
+    #region Interceptors
+
+    public class MethodInterceptor_EntryAttribute : Attribute, IMethodInterceptor
+    {
+        public void OnEnter(Type declaringType, object instance, MethodBase methodbase, object[] values) => throw new MethodInterceptor_Exception();
+
+        public void OnException(Exception e)
+        {
+        }
+
+        public void OnExit()
+        {
+        }
+    }
+
+    public class MethodInterceptor_Exception : Exception
+    {
+    }
+
+    public class MethodInterceptor_ExceptionAttribute : Attribute, IMethodInterceptor
+    {
+        public void OnEnter(Type declaringType, object instance, MethodBase methodbase, object[] values)
+        {
+        }
+
+        public void OnException(Exception e) => throw new MethodInterceptor_Exception();
+
+        public void OnExit()
+        {
+        }
+    }
+
+    public class MethodInterceptor_ExitAttribute : Attribute, IMethodInterceptor
+    {
+        public void OnEnter(Type declaringType, object instance, MethodBase methodbase, object[] values)
+        {
+        }
+
+        public void OnException(Exception e)
+        {
+        }
+
+        public void OnExit() => throw new MethodInterceptor_Exception();
+    }
+
+    #endregion Interceptors
+
+    [TestClass]
+    public class Method_Interceptor_Tests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(MethodInterceptor_Exception))]
+        public void Method_Interceptor_Invoke_OnEnter_Test() => this.EnterInvoke();
+
+        [TestMethod]
+        [ExpectedException(typeof(MethodInterceptor_Exception))]
+        public void Method_Interceptor_Invoke_OnException_Test() => this.ExitInvoke();
+
+        [TestMethod]
+        [ExpectedException(typeof(MethodInterceptor_Exception))]
+        public void Method_Interceptor_Invoke_OnExit_Test() => this.ExitInvoke();
+
+        #region Intercepted Methods
+
+        [MethodInterceptor_Entry]
+        private void EnterInvoke()
+        {
+        }
+
+        [MethodInterceptor_Exception]
+        private void ExceptionInvoke()
+        {
+        }
+
+        [MethodInterceptor_Exit]
+        private void ExitInvoke()
+        {
+        }
+
+        #endregion Intercepted Methods
+    }
+}
