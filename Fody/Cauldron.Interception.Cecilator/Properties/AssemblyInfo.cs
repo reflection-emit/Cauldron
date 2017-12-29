a508bc77cd4b48a5901449c809550c0ce68fa227
Alexei Stryker <alexander.schunk@capgemini.com>
2017-12-29 21:14:35 +0100

    documentation updated ... and version up

--- Fody/Cauldron.Interception.Cecilator/Properties/AssemblyInfo.cs
+++ Fody/Cauldron.Interception.Cecilator/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //
 // Major Version Minor Version Build Number Revision
 //
-[assembly: AssemblyVersion("2.0.0.28")]
-[assembly: AssemblyFileVersion("2.0.0.28")]
+[assembly: AssemblyVersion("2.0.0.29")]
+[assembly: AssemblyFileVersion("2.0.0.29")]
