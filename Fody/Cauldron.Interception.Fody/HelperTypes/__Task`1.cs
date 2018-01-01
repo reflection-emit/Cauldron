8bfac9f441abf1d57f7550f32d95699b7e92a2fb
Alexei Stryker <alexander.schunk@capgemini.com>
2018-01-01 19:02:57 +0100

    ...

--- Fody/Cauldron.Interception.Fody/HelperTypes/__Task`1.cs
+++ Fody/Cauldron.Interception.Fody/HelperTypes/__Task`1.cs
@@ -2,7 +2,7 @@
 
 namespace Cauldron.Interception.Fody.HelperTypes
 {
-    [HelperTypeName("System.Threading.Tasks.Task`1")]
+    [HelperTypeName("System.Threading.Tasks.Task`1", "System.Threading.Tasks.Task`1", "system.threading.tasks")]
     public sealed class __Task_1 : HelperTypeBase<__Task_1>
     {
         [HelperTypeMethod("FromResult", 1)]
