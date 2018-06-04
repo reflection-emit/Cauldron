16c87f2f03fc07b3d8c477844f9770e3d8c54d0b
reflection-emit <shinsen@outlook.com>
2018-06-04 12:44:01 +0200

    version up and documentaion update

--- Fody/Cauldron.Interception.Cecilator/Properties/AssemblyInfo.cs
+++ Fody/Cauldron.Interception.Cecilator/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //
 // Major Version Minor Version Build Number Revision
 //
-[assembly: AssemblyVersion("3.0.0.21")]
-[assembly: AssemblyFileVersion("3.0.0.21")]
+[assembly: AssemblyVersion("3.0.0.22")]
+[assembly: AssemblyFileVersion("3.0.0.22")]
