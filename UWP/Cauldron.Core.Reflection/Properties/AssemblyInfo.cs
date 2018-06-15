782446afc4172c75e549cc87ff3f5c485856d830
reflection-emit <shinsen@outlook.com>
2018-06-15 10:18:14 +0200

    version up

--- UWP/Cauldron.Core.Reflection/Properties/AssemblyInfo.cs
+++ UWP/Cauldron.Core.Reflection/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.0.27")]
+// [assembly: AssemblyVersion("3.0.0.28")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.27")]
+[assembly: AssemblyFileVersion("3.0.0.28")]
 [assembly: ComVisible(false)]
--- Win32/Cauldron.Activator/Properties/AssemblyInfo.cs
+++ Win32/Cauldron.Activator/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // "3.0.0.8")]
-// [assembly: AssemblyVersion("3.0.0.27")]
+// [assembly: AssemblyVersion("3.0.0.28")]
 [assembly: AssemblyVersion("3.0.0.22")]
-[assembly: AssemblyFileVersion("3.0.0.27")]
+[assembly: AssemblyFileVersion("3.0.0.28")]
