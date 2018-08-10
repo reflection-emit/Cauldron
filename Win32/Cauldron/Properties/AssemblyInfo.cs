8b7d28ea48962034547b697dcab58cbe91c1b0c7
reflection-emit <shinsen@outlook.com>
2018-08-10 14:15:16 +0200

    ...

--- Win32/Cauldron/Properties/AssemblyInfo.cs
+++ Win32/Cauldron/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // "3.0.0.8")]
-[assembly: AssemblyVersion("3.1.0.34")]
-[assembly: AssemblyFileVersion("3.1.0.34")]
+[assembly: AssemblyVersion("3.1.0.38")]
+[assembly: AssemblyFileVersion("3.1.0.38")]
