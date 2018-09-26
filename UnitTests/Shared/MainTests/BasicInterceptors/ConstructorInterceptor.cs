78510138109e7155d5fce079f128712fd89941f9
reflection-emit <alexander.schunk@ikea.com>
2018-09-27 01:53:39 +0200

    #70 fixed - Very dumb error

---
+++ UnitTests/Shared/MainTests/BasicInterceptors/ConstructorInterceptor.cs
@@ -0,0 +1,30 @@
+﻿using Cauldron.Interception;
+using System;
+using System.Reflection;
+
+namespace MainTests.BasicInterceptors
+{
+    [AttributeUsage(AttributeTargets.Constructor)]
+    public sealed class ConstructorInterceptor : Attribute, IConstructorInterceptor
+    {
+        public void OnBeforeInitialization(Type declaringType, MethodBase methodbase, object[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnEnter(Type declaringType, object instance, MethodBase methodbase, object[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OnException(Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnExit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/Shared/MainTests/BasicInterceptors/Ctor_TestClasses.cs
@@ -0,0 +1,10 @@
+﻿namespace MainTests.BasicInterceptors
+{
+    public class Ctor_TestClass1
+    {
+        [ConstructorInterceptor]
+        public Ctor_TestClass1()
+        {
+        }
+    }
+}
