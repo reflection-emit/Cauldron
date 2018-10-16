4f6a52cef4481ac45036a4dbd924d4771a3b8619
reflection-emit <alexander.schunk@ikea.com>
2018-10-16 21:59:52 +0200

    Version upgrade Docs updated

--- Win32/Cauldron.Interception/Properties/AssemblyInfo.cs
+++ Win32/Cauldron.Interception/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // "3.0.0.8")]
-[assembly: AssemblyVersion("3.2.0.1")]
-[assembly: AssemblyFileVersion("3.2.0.1")]
+[assembly: AssemblyVersion("3.2.0.2")]
+[assembly: AssemblyFileVersion("3.2.0.2")]
