7238da0981b6956f10f78387f390cf7bc7badf26
Delmoskito <35399716+Delmoskito@users.noreply.github.com>
2018-10-22 09:03:51 +0200

    Added missing ICauldronUICommand

---
+++ Win32/Cauldron.Win32.WPF/ICauldronUICommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Cauldron.XAML
+{
+    /// <summary>
+    /// Represents a UI command.
+    /// </summary>
+    public interface ICauldronUICommand
+    {
+        /// <summary>
+        /// The label for command
+        /// </summary>
+        string Label { get; }
+
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        void Invoke();
+    }
+}
